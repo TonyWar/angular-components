ad84ab6c7514836c3b53d75c5aab555b78ed4447
Tony Varlamov <tonywarlamoff@gmail.com>
2019-05-14 10:50:57 +0300

    fix

--- src/app/form/custom-forms/components/amount-form/amount-form.component.spec.ts
+++ src/app/form/custom-forms/components/amount-form/amount-form.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AmountFormComponent } from './amount-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+// tslint:disable-next-line: no-implicit-dependencies
 import { CustomControlsModule } from 'src/app/form/custom-controls/custom-controls.module';
 
 describe('AmountFormComponent', () => {
