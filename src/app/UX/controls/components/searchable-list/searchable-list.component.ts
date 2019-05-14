c8ddc6b869fe21d7c89fee103af406545d523f2c
Dima <purisdima@mail.ru>
2019-05-14 10:00:23 +0300

    add searchable  component

---
+++ src/app/UX/controls/components/searchable-list/searchable-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchable-list',
+  templateUrl: './searchable-list.component.html',
+  styleUrls: ['./searchable-list.component.less']
+})
+export class SearchableListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/UX/controls/controls.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchableListComponent } from './components/searchable-list/searchable-list.component';
+
+@NgModule({
+  declarations: [SearchableListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ControlsModule { }
