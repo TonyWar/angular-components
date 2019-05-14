67842cebe065d564e4ab5e150819e25dd56a7ba7
Dima <purisdima@mail.ru>
2019-05-14 15:35:50 +0300

    upd searchable list

---
+++ src/app/UX/controls/components/list-item/list-item.component.ts
@@ -0,0 +1,38 @@
+import { Component,  Input, HostBinding, Output, EventEmitter } from '@angular/core';
+import { Highlightable } from '@angular/cdk/a11y';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.less']
+})
+export class ListItemComponent implements Highlightable {
+  @Input() item: any;
+  public _isActive = false;
+
+  @HostBinding('class.active') get isActive() {
+    return this._isActive;
+  };
+
+  @Output() customClick = new EventEmitter<string>();
+  handleClick = (e: any) => {
+    console.log('Clicked item', this.item)
+    this.customClick.emit(this.item.id);
+  }
+
+  setActiveItem() {
+    this.setActiveStyles();
+  }
+
+  setActiveStyles() {
+    this._isActive = true;
+  };
+
+  setInactiveStyles() {
+    this._isActive = false;
+  }
+
+  getLabel() {
+    return this.item.name;
+  }
+}
--- src/app/UX/controls/components/searchable-list/searchable-list.component.ts
+++ src/app/UX/controls/components/searchable-list/searchable-list.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChildren, QueryList, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { ListItemComponent } from '../list-item/list-item.component';
+import { ActiveDescendantKeyManager } from '@angular/cdk/a11y';
+import { ENTER } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-searchable-list',
   templateUrl: './searchable-list.component.html',
   styleUrls: ['./searchable-list.component.less']
 })
-export class SearchableListComponent implements OnInit {
+export class SearchableListComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChildren(ListItemComponent) items: QueryList<ListItemComponent> | undefined;
+  users = [
+    {
+      "id": "5b902934d965e7501f4e1c6f",
+      "name": "Caroline Hodges"
+    },
+    {
+      "id": "5b9029348f7eed8b6f5f02db",
+      "name": "Delores Rivas"
+    },
+    {
+      "id": "5b9029346f48c8407c64d0d5",
+      "name": "Darlene Franklin"
+    },
+    {
+      "id": "5b9029341eff315fa87f9e21",
+      "name": "Alfreda Love"
+    },
+    {
+      "id": "5b9029342e8917c6ccdb9865",
+      "name": "Marcy Ratliff"
+    },
+    {
+      "id": "5b9029349dbb48013460e01f",
+      "name": "Beulah Nielsen"
+    },
+    {
+      "id": "5b902934f4f1586e5e72d74a",
+      "name": "Morton Kerr"
+    },
+    {
+      "id": "5b9029347918bb204bf7014e",
+      "name": "Autumn Tillman"
+    },
+    {
+      "id": "5b902934b86f80e1fc60c626",
+      "name": "Diane Bennett"
+    },
+    {
+      "id": "5b9029348999f59215020349",
+      "name": "June Eaton"
+    }
+  ]
 
-  ngOnInit() {
+  private keyManager: ActiveDescendantKeyManager<ListItemComponent> | undefined;
+  model = '';
+  public active: boolean = false;
+
+  @ViewChild('input') input?: ElementRef;
+
+  ngAfterViewInit() {
+    this.keyManager = new ActiveDescendantKeyManager(this.items!).withWrap()
+      .withTypeAhead();
   }
 
+  onKeydown(event: any) {
+    if (event.keyCode === ENTER) {
+      this.model = this.keyManager!.activeItem!.getLabel();
+    } else {
+      this.keyManager!.onKeydown(event);
+    }
+  }
+
+  handleCustomClick(id: string) {
+    console.log('In handleCustomClick')
+    const index = this.users.findIndex(user => user.id === id);
+    this.keyManager!.setActiveItem(index);
+    console.log('Active item', this.keyManager!.activeItem, 'id', id, 'index', index)
+    this.model = this.keyManager!.activeItem!.getLabel();
+    this.input!.nativeElement.focus();
+  }
 }
